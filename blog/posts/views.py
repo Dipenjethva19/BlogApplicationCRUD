2376d5e4759a382167287af28b2ed55d84b81543
dipenjethva19 <dipenjethva19@gmail.com>
2016-09-27 23:44:54 -0700

    After dynamic url with slug.

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -31,9 +31,9 @@ def post_create(request):
     return render(request, "post_form.html", context)
 
 
-def post_detail(request, id=None):  # retrive
+def post_detail(request, slug=None):  # retrive
     # instance = Post.objects.get(id=2)
-    instance = get_object_or_404(Post, id=id)
+    instance = get_object_or_404(Post, slug=slug)
     # instance = get_object_or_404(Post, title="Hello,")
     context = {
         "title": instance.title,
@@ -75,8 +75,8 @@ def post_list(request):  # list_iteams
     return render(request, "post_list.html", context)
 
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -92,8 +92,8 @@ def post_update(request, id=None):
     return render(request, "post_form.html", context)
 
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully Deleted")
     return redirect("posts:home")
